c717aa611d4c4315f701c289d862a63c79047866
Hiroki MATSUMOTO <e125705@ie.u-ryukyu.ac.jp>
2016-06-24 11:54:55 +0900

    delete comment

--- Horatarea/ViewController.swift
+++ Horatarea/ViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 
 class ViewController: UIViewController {
      
-        //test comment hirotaka
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,13 +22,7 @@ class ViewController: UIViewController {
         
         
         
-        
-        //test byincho
     }
 
 
 }
-
-
-
-//teeeeeeest
